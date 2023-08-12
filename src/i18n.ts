8c8cecc75ae85f8417c0c5202dc45b6a6acb2997
ibuki2003 <main@fuwa.dev>
2023-08-13 02:55:10 +0900

    refactor: prettier

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import { initReactI18next, useTranslation } from "react-i18next";
+import { getLocales } from "react-native-localize";
 import en from "@/locales/en";
 import ja from "@/locales/ja";
-import { getLocales } from "react-native-localize";
 import i18n from "i18next";
 
 i18n.use(initReactI18next).init({
