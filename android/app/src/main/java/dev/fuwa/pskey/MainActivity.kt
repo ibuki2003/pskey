6b8e9c9352dda23813ee6c58ffa667553dd610cb
ibuki2003 <main@fuwa.dev>
2023-08-13 03:25:31 +0900

    build: convert java files to kotlin

---
+++ android/app/src/main/java/dev/fuwa/pskey/MainActivity.kt
@@ -0,0 +1,33 @@
+package dev.fuwa.pskey
+
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String? {
+        return "pskey"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. Here we use a util class [ ] which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return DefaultReactActivityDelegate(
+            this,
+            mainComponentName!!,  // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+            fabricEnabled
+        )
+    }
+}
