c5bc865db7995cefc1340cd1995dfcb8c7ab07a2
ibuki2003 <main@fuwa.dev>
2023-09-01 22:48:40 +0900

    fix: locale type

--- src/locales/type.ts
+++ src/locales/type.ts
@@ -1,4 +1,5 @@
 import ja from "./ja";
 
-type Locale = Record<keyof typeof ja, string>;
+type L<T> = { [K in keyof T]: T[K] extends string ? string : L<T[K]> };
+type Locale = L<typeof ja>;
 export default Locale;
