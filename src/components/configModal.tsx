873c5c037c35244d956350fb72c25a93267c0300
ibuki2003 <main@fuwa.dev>
2023-11-01 23:03:30 +0900

    release v1.6.0

--- src/components/configModal.tsx
+++ src/components/configModal.tsx
@@ -162,7 +162,7 @@ const ConfigModal: React.FC<Props> = (props) => {
         source={{ uri: "splash_icon" }}
         style={{ width: 100, height: 100 }}
       />
-      <Text style={[style_fg, styles.headingText]}>PSkey v1.5.0</Text>
+      <Text style={[style_fg, styles.headingText]}>PSkey v1.6.0</Text>
       <Text style={[style_fg, styles.noteText]}>{t("aboutPSkey")}</Text>
       <Pressable
         onPress={() => {
